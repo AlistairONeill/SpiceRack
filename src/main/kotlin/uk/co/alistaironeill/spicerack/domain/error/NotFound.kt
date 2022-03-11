b84894af2f0dbef561d73e66a0c2e59ccdcff0c8
alistaironeill <me@alistaironeill.co.uk>
2022-03-11 00:52:14 +0000

    Added fn for NotFound$Colour

--- src/main/kotlin/uk/co/alistaironeill/spicerack/domain/error/NotFound.kt
+++ src/main/kotlin/uk/co/alistaironeill/spicerack/domain/error/NotFound.kt
@@ -1,5 +1,11 @@
 package uk.co.alistaironeill.spicerack.domain.error
 
+import uk.co.alistaironeill.spicerack.domain.colour.Colour
+
 data class NotFound(val type: String, val id: String): AonError {
     override val msg = "Could not find $type[$id]"
+
+    companion object {
+        operator fun invoke(colour: Colour) = NotFound("Colour", colour.name)
+    }
 }
--- src/test/kotlin/uk/co/alistaironeill/spicerack/domain/error/NotFoundTest.kt
+++ src/test/kotlin/uk/co/alistaironeill/spicerack/domain/error/NotFoundTest.kt
@@ -3,12 +3,23 @@ package uk.co.alistaironeill.spicerack.domain.error
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import uk.co.alistaironeill.spicerack.domain.colour.Colour
 
 class NotFoundTest {
     @Test
     fun `returns the correct error message`() {
         expectThat(NotFound("Foo", "bar")) {
             get { msg }.isEqualTo("Could not find Foo[bar]")
+        }
+    }
+
+    @Test
+    fun `can create from Colour`() {
+        val colour = Colour.values().random()
+
+        expectThat(NotFound(colour)) {
+            get { type }.isEqualTo("Colour")
+            get { id }.isEqualTo(colour.name)
         }
     }
 }
